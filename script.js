5adce6d0c738b4fd91438d8c2c91d266b3c6872c
guilhermeicarofr <guilhermeicarofr@gmail.com>
2022-06-09 23:23:56 -0300

    implementado query seletor para todos os cards com estrutura if-switch

--- script.js
+++ script.js
@@ -1,3 +1,63 @@
-function selectItem (nome, valor) {
-    return nome;
+function selectItem (m, c) {
+    
+
+    let card;
+    if (m===1) {
+        switch(c) {
+            case 1:
+                card = document.querySelector(".menu:nth-of-type(1) div:nth-child(1)");
+                break;
+            case 2:
+                card = document.querySelector(".menu:nth-of-type(1) div:nth-child(2)");
+                break;        
+            case 3:
+                card = document.querySelector(".menu:nth-of-type(1) div:nth-child(3)");
+                break;
+            case 4:
+                card = document.querySelector(".menu:nth-of-type(1) div:nth-child(4)");
+                break;
+        }
+    } else if (m===2) {
+        switch(c) {
+            case 1:
+                card = document.querySelector(".menu:nth-of-type(2) div:nth-child(1)");
+                break;
+            case 2:
+                card = document.querySelector(".menu:nth-of-type(2) div:nth-child(2)");
+                break;        
+            case 3:
+                card = document.querySelector(".menu:nth-of-type(2) div:nth-child(3)");
+                break;
+            case 4:
+                card = document.querySelector(".menu:nth-of-type(2) div:nth-child(4)");
+                break;
+        }
+    } else if (m===3) {
+        switch(c) {
+            case 1:
+                card = document.querySelector(".menu:nth-of-type(3) div:nth-child(1)");
+                break;
+            case 2:
+                card = document.querySelector(".menu:nth-of-type(3) div:nth-child(2)");
+                break;        
+            case 3:
+                card = document.querySelector(".menu:nth-of-type(3) div:nth-child(3)");
+                break;
+            case 4:
+                card = document.querySelector(".menu:nth-of-type(3) div:nth-child(4)");
+                break;
+        }
+    }
+    card.classList.toggle("selected");
+
+
+
+
+
+
+
+
+
+
+
 }
