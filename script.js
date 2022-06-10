e1197800d2419a8b3e115377048f10a57abb7a14
guilhermeicarofr <guilhermeicarofr@gmail.com>
2022-06-10 02:14:07 -0300

    criacao de diversas funcoes no js para selecao dos cards e captura do nome e valor de cada item diretamente do html, inicio da criacao da checkout box

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-function selectItem (m, c) {
-    
-
+function findCard (m, c) {
     let card;
     if (m===1) {
         switch(c) {
@@ -48,16 +46,54 @@ function selectItem (m, c) {
                 break;
         }
     }
+    return card;
+}
+
+function markCard (m,c) {
+    const card = findCard(m,c);
     card.classList.toggle("selected");
+}
+
+function cardPrice (m,c) {
+    let text = findCard(m,c).querySelector("h3").innerHTML;
+    let price='';
+
+    for(let i=2;i<7;i++) {
+
+        if (text[i]==',')
+            price += '.';
+        else
+            price += text [i];
+    }
+    Number(price);
+    return(price);
+}
+
+function cardName (m,c) {
+    const name = findCard(m,c).querySelector("h2").innerHTML;
+    return name;
+}
+
+function onlyCardMenu (m) {
+
+    let card;
+    for (let j=1;j<5;j++) {
+        card = findCard(m,j);
+        if (card.classList.contains("selected"))
+            card.classList.toggle("selected");
+    }
+} 
+
+function buttonReady () {
+    const button = document.querySelector(".bottom-bar button");
+    button.classList.add("ready");
+    button.querySelector("p").innerHTML = "Fechar pedido";
+    return true;
+}
 
 
 
-
-
-
-
-
-
-
-
+function selectItem (m, c) {
+    onlyCardMenu (m)
+    markCard(m,c);
 }
