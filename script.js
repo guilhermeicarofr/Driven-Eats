9cc83e78ce4d19a2254b12d010f240b2bbabef73
guilhermeicarofr <guilhermeicarofr@gmail.com>
2022-06-11 01:43:03 -0300

    implementacao da tela de chekout(sem tabela) chamada no botao de finalizar, coleta do nome e endereço do usuario, botao de checkout com formatacao da mensagem de whatapp ok

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let item=[]; //array contendo os nome dos 3 produtos selecionados
 let price=[]; //array contendo os precos dos 3 produtos selecionados
-let count = 0;
+let count = 0; //contador de pedidos selecionados
+let client = ''; //nome do cliente
+let adress = ''; //endereco do cliente
+let wmessage = ''; //mensagem do whatsapp nao codificada
+
+/*funcoes secundárias chamadas por outras funcoes*/
 
 //marca o card de produto selecionado
 function markCard (card) {
@@ -28,9 +33,8 @@ function onlyCardMenu (m) {
 
 //extrai o preco do produto do html
 function cardPrice (card) {
-    let text = card.querySelector("h3").innerHTML;
+    const text = card.querySelector("h3").innerHTML;
     let price='';
-
     for(let i=2;i<7;i++) {
 
         if (text[i]==',')
@@ -38,7 +42,7 @@ function cardPrice (card) {
         else
             price += text [i];
     }
-    Number(price);
+    price = Number(price);
     return(price);
 }
 
@@ -53,16 +57,36 @@ function buttonReady () {
     const button = document.querySelector(".bottom-bar button");
     button.classList.add("ready");
     button.querySelector("p").innerHTML = "Fechar pedido";
-    return true;
+}
+
+//funcao que chama/volta a tela de confirmacao
+function checkout () {
+    document.querySelector(".checkout-box").classList.toggle('hidden');
+}
+
+//funcao que escreve a mensagem de whatsapp e codifica ela para o link
+function whatsWrite () {
+    let total = (price[0]+price[1]+price[2]);
+    wmessage =`Olá, gostaria de fazer o pedido:\n - Prato: ${item[0]}\n - Bebida: ${item[1]}\n - Sobremesa: ${item[2]}\nTotal: R$ ${total.toFixed(2)}\n\nNome: ${client}\nEndereço: ${adress}`;
+    return (encodeURIComponent(wmessage));
+}
+
+//funcao que modifica o href do link do botao da tela de chekout para formatar o whatsapp
+function whatsLink () {
+    let link = `https://wa.me/5531993751892?text=${whatsWrite()}`;
+    let html = `<a href="${link}" target="_blank">Tudo certo, pode pedir!</a>`;
+    (document.querySelector(".checkout-box div button:nth-of-type(1)").innerHTML) = html;
 }
 
 
 
-//Funcao chamada pelo clique na div de um card de produto
+/*funcoes primarias chamadas por clique*/
+
+//Funcao chamada pelo clique na div de um card de produto, recebe o numero do menu(1,2,3) e o 'div.this'
 function selectItem (m, card) {
     
     //chamando funcoes para desmarcar outro pedido do mesmo menu e marcar o clicado
-    onlyCardMenu (m);
+    onlyCardMenu(m);
     markCard(card);
 
     //verifica se o item clicado é apenas uma troca de opcao ou é mais uma selecao de outro menu
@@ -76,4 +100,15 @@ function selectItem (m, card) {
     //monitora se os 3 itens já foram selecionados para finalizar o pedido
     if (count>=3)
         buttonReady();
+}
+
+//funcao chamada pelo botao finalizar pedido, pede nome e endereco, armazena e chama a tela de confirmacao
+function closeOrder (button) {
+    
+    if (button.classList.contains("ready")) {
+        client = prompt("Informe seu nome:");
+        adress = prompt("Informe seu endereço");
+        checkout();
+        whatsLink();
+    }
 }
